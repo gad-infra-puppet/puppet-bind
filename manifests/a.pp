8b32c6b812acaaf520de767d8f1e0d014f470842
Mingxiang.Zhang <ming.zhang@gree.net>
2013-01-29 16:17:49 +0900

    change host=> host.domain. for ptr record

--- manifests/a.pp
+++ manifests/a.pp
@@ -3,42 +3,40 @@
 # Creates an IPv4 record.
 #
 # Arguments:
-#  *$zone*:       Bind::Zone name
-#  *zone_arpa*:   needed if you set $ptr to true
-#  *$ptr*:        set it to true if you want the related PTR records
-#                 NOTE: don't forget to create the zone!
+# *$zone*:  Bind::Zone name
+# *$owner*: owner of the Resource Record
+# *$host*:  target of the Resource Record
+# *$ttl*:   Time to Live for the Resource Record. Optional.
+# *$ptr*:   create the corresponding ptr record (default=false)
 #
-#  For other arguments, please refer to bind::records !
 #
 define bind::a(
   $zone,
-  $hash_data,
+  $host,
   $ensure = present,
-  $zone_arpa = '',
-  $ptr    = true,
-  $content_template = false,
+  $owner  = false,
+  $ttl    = false,
+  $ptr    = false
 ) {
 
-  if ($ptr and $zone_arpa == '') {
-    fail 'You need zone_arp if you want the PTR!'
-  }
-
   bind::record {$name:
-    ensure           => $ensure,
-    zone             => $zone,
-    hash_data        => $hash_data,
-    record_type      => 'A',
-    content_template => $content_template,
+    ensure      => $ensure,
+    zone        => $zone,
+    owner       => $owner,
+    host        => $host,
+    ttl         => $ttl,
+    record_type => 'A',
   }
 
   if $ptr {
-    bind::record {"PTR ${name}":
-      ensure           => $ensure,
-      zone             => $zone_arpa,
-      record_type      => 'PTR',
-      ptr_zone         => $zone,
-      hash_data        => $hash_data,
-      content_template => $content_template,
+    $arpa      = inline_template("<%= require 'ipaddr'; IPAddr.new(host).reverse %>")
+    $arpa_zone = inline_template("<%= require 'ipaddr'; IPAddr.new(host).reverse.split('.')[1..-1].join('.') %>")
+
+    bind::ptr {"${arpa}.":
+      ensure => $ensure,
+      zone   => $arpa_zone,
+      host   => "$name.$zone.",
+      ttl    => $ttl,
     }
   }
 
